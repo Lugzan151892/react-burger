097b74439b3fe07e1e5a163a65897ac2c0b25250
Denis Gukov <lgzn151892@gmail.com>
2023-01-06 11:47:21 +0300

    feat: added markup for order list on main page

---
+++ src/components/OrderListElement/OrderListElement.jsx
@@ -0,0 +1,40 @@
+import styles from './OrderListElement.module.css';
+import {CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+
+function OrderListElement({ item }) {
+    const ingridientsList = item.ingridients.slice(0, 5);
+    const extraItem = item.ingridients[5];
+    const amountOfExtraElements = item.ingridients.slice(5, item.ingridients.length).length;
+    
+    return (
+        <div className={`${styles.order_element} mr-2 mb-4`}>
+            <div className={`${styles.description} mt-6 ml-6 mr-6`}>
+                <p className='text text_type_digits-default'>{item.number}</p>
+                <p className='text text_type_main-small text_color_inactive'>{item.time}</p>
+            </div>
+            <h2 className='text text_type_main-medium m-6'>{item.title}</h2>
+            <div className={`${styles.ingridients} m-6`}>
+                <div className={styles.images}>
+                    {ingridientsList.map((el, index) => (                        
+                        <img className={styles.image} style={{ left:`-${index*48}px`, zIndex:`${5-index}`}} src={el.image_mobile} alt={el.name} key={el._id}/>
+                    ))}
+                    {
+                        extraItem ? (
+                            <div className={styles.extra_image_container} style={{left:`-${5*48}px`, zIndex:'0'}}>
+                                <div className={styles.extra_number}>{`+${amountOfExtraElements}`}</div>
+                                <img className={styles.extra_image} src={extraItem.image_mobile} alt={extraItem.name}/>
+                            </div>
+                            
+                        ) : null
+                    }
+                </div>
+                <div className={styles.price}>
+                    <p className='text text_type_digits-default mr-2'>{item.price}</p>
+                    <CurrencyIcon type="primary" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OrderListElement;
--- src/pages/OrderList.jsx
+++ src/pages/OrderList.jsx
@@ -1,10 +1,52 @@
-
+import testObjects from "../utils/testData";
+import styles from './OrderList.module.css';
+import OrderListElement from "../components/OrderListElement/OrderListElement";
 
 function OrderList() {
-    return (
-        <div>
-            <h1>В разработке</h1>
-        </div>
+    const numbers = {
+        ready: ['034533', '034532', '034534', '034537', '034539'],
+        preparing: ['034533', '034532', '034534', '034537', '034539'],
+        total: 28752,
+        today: 138,
+    }
+
+    return (   
+        <div className={styles.container}>     
+            <h1>Лента заказов</h1>
+            <div className={styles.orders}>
+                <div className={`${styles.orderlist} mr-15`}>
+                    {
+                        testObjects.map((el, index)=>(
+                            <OrderListElement item={el} key={index}/>
+                        ))
+                    }                
+                </div>
+                <div>
+                    <div className={styles.orders_numbers}>
+                        <div className={`${styles.orders_container} mr-9`}>
+                            <h2 className="text text_type_main-medium mb-6">Готовы:</h2>
+                            {
+                                numbers.ready.map((el)=> (
+                                    <p className="text text_type_digits-default mb-2" key={el}>{el}</p>
+                                ))
+                            }
+                        </div>
+                        <div className={styles.orders_container}>
+                            <h2 className="text text_type_main-medium mb-6">В работе:</h2>
+                            {
+                                numbers.preparing.map((el)=> (
+                                    <p className="text text_type_digits-default mb-2" key={el}>{el}</p>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <h2 className="text text_type_main-medium mt-15">Выполнено за все время:</h2>
+                    <p className={`${styles.numbers} text text_type_digits-large`}>{numbers.total}</p>
+                    <h2 className="text text_type_main-medium mt-15">Выполнено за сегодня:</h2>
+                    <p className={`${styles.numbers} text text_type_digits-large`}>{numbers.today}</p>
+                </div>
+            </div>
+        </div> 
     )
 }
 
